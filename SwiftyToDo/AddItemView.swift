43471be9ddec52cefcf8887df74f9324bf647f6b
DrBeta <freedom>
2019-07-13 14:25:27 -0500

    Created a new view for the adding of a task

---
+++ SwiftyToDo/AddItemView.swift
@@ -0,0 +1,23 @@
+//
+//  AddItemView.swift
+//  SwiftyToDo
+//
+//  Created by freedom on 7/13/19.
+//  Copyright © 2019 Pykes. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddItemView : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct AddItemView_Previews : PreviewProvider {
+    static var previews: some View {
+        AddItemView()
+    }
+}
+#endif
